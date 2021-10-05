03ff3f7b8a6be9df6f5284a7f60dab339cddf45c
Laura Pohl <77807353+irishmaiden18@users.noreply.github.com>
2021-10-05 13:07:12 -0400

    Add files via upload

--- pacmen.js
+++ pacmen.js
@@ -1,7 +1,7 @@
 let pos = 0;
 const pacArray = [
-  ['images/pacman1.png', 'images/pacman2.png'],
-  ['images/pacman3.png', 'images/pacman4.png'],
+  ['/images/pacman1.png', '/images/pacman2.png'],
+  ['/images/pacman3.png', '/images/pacman4.png'],
 ];
 let direction = 0;
 const pacMen = []; // This array holds all the pacmen
@@ -24,7 +24,7 @@ function makePac() {
   let game = document.getElementById('game');
   let newimg = document.createElement('img');
   newimg.style.position = 'absolute';
-  newimg.src = 'images/pacman1.png';
+  newimg.src = '/images/pacman1.png';
   newimg.width = 100;
 
   // TODO: set position here
